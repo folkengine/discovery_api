963328af6542968a43390e2597d93a4f337a24e7
ManApart <akucera@pillartechnology.com>
2019-10-04 14:00:18 -0400

    event listener saves organizations

---
+++ test/unit/discovery_api/data/event_handler_test.exs
@@ -0,0 +1,34 @@
+defmodule DiscoveryApi.EventListenerTest do
+  use ExUnit.Case
+  use Placebo
+
+  import SmartCity.Event, only: [organization_update: 0, data_ingest_start: 0]
+
+  alias SmartCity.TestDataGenerator, as: TDG
+  alias DiscoveryApi.EventHandler
+  alias DiscoveryApi.Schemas.Organizations
+
+  describe "handle_event/1" do
+    test "does not save non-organization to ecto" do
+      org = TDG.create_organization(%{})
+      allow(Organizations.create_or_update(any()), return: :dontcare)
+
+      Brook.Test.with_event(:discovery_api_brook, fn ->
+        EventHandler.handle_event(Brook.Event.new(type: data_ingest_start(), data: org, author: :author))
+      end)
+
+      assert_called(Organizations.create_or_update(any()), times(0))
+    end
+
+    test "should save organization to ecto" do
+      org = TDG.create_organization(%{})
+      allow(Organizations.create_or_update(any()), return: :dontcare)
+
+      Brook.Test.with_event(:discovery_api_brook, fn ->
+        EventHandler.handle_event(Brook.Event.new(type: organization_update(), data: org, author: :author))
+      end)
+
+      assert_called(Organizations.create_or_update(org))
+    end
+  end
+end
