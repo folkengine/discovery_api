0d3a5bf6976479109c464ef23d9752defe8083b4
ManApart <akucera@pillartechnology.com>
2019-10-04 10:01:42 -0400

    ecto complete

--- test/integration/discovery_api/schemas/organizations_test.exs
+++ test/integration/discovery_api/schemas/organizations_test.exs
@@ -7,12 +7,18 @@ defmodule DiscoveryApi.Schemas.OrganizationsTest do
   alias DiscoveryApi.Schemas.Organizations
   alias DiscoveryApi.Schemas.Organizations.Organization
 
+  @org_id "12343532"
+
+  setup do
+    Repo.delete_all(Organization)
+
+    :ok
+  end
+
   describe "create_or_update/2" do
     test "creates an organization" do
-      org_id = "1"
-
       org = %{
-        org_id: org_id,
+        org_id: @org_id,
         name: "a",
         title: "b",
         description: "description",
@@ -20,9 +26,9 @@ defmodule DiscoveryApi.Schemas.OrganizationsTest do
         logo_url: "logo"
       }
 
-      assert {:ok, saved} = Organizations.create_or_update(org_id, org)
+      assert {:ok, saved} = Organizations.create_or_update(@org_id, org)
 
-      actual = Organizations.get_organization(org_id)
+      actual = Organizations.get_organization(@org_id)
       assert !is_nil(actual)
       assert org.org_id == actual.org_id
       assert org.name == actual.name
@@ -33,31 +39,38 @@ defmodule DiscoveryApi.Schemas.OrganizationsTest do
     end
 
     test "updates an organization" do
-      org_id = "2"
 
-      assert {:ok, created} = Organizations.create_or_update(org_id, %{name: "a", title: "b"})
-      assert {:ok, updated} = Organizations.create_or_update(org_id, %{name: "c", title: "d"})
+      assert {:ok, created} = Organizations.create_or_update(@org_id, %{name: "a", title: "b"})
+      assert {:ok, updated} = Organizations.create_or_update(@org_id, %{name: "c", title: "d"})
 
-      actual = Organizations.get_organization(org_id)
+      actual = Organizations.get_organization(@org_id)
       assert !is_nil(actual)
       assert "c" == actual.name
       assert "d" == actual.title
     end
 
-    # test "returns an error when org id is not provided for a new org" do
-    #   assert {:error, changeset} = Organizations.create_or_update(nil, %{name: "a", title: "b"})
+    test "returns an error when name is not provided" do
 
-    #   assert changeset.errors |> Keyword.has_key?(:org_id)
-    #   assert nil == Repo.get_by(User, org_id: nil)
-    # end
+      assert {:error, changeset} = Organizations.create_or_update(@org_id, %{title: "title"})
+
+      assert changeset.errors |> Keyword.has_key?(:name)
+      assert is_nil(Organizations.get_organization(@org_id))
+    end
+
+    test "returns an error when title is not provided" do
+
+      assert {:error, changeset} = Organizations.create_or_update(@org_id, %{name: "name"})
+
+      assert changeset.errors |> Keyword.has_key?(:title)
+      assert is_nil(Organizations.get_organization(@org_id))
+    end
   end
 
   describe "create_or_update/1" do
     test "creates an organization from a smart city struct" do
-      org_id = "3"
 
       org = %SmartCity.Organization{
-        id: org_id,
+        id: @org_id,
         orgName: "a",
         orgTitle: "b",
         description: "description",
@@ -67,7 +80,7 @@ defmodule DiscoveryApi.Schemas.OrganizationsTest do
 
       assert {:ok, saved} = Organizations.create_or_update(org)
 
-      actual = Organizations.get_organization(org_id)
+      actual = Organizations.get_organization(@org_id)
       assert !is_nil(actual)
       assert org.id == actual.org_id
       assert org.orgName == actual.name
