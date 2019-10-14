6b9e6ef5e936e52f94dffe7a0bd4e4834903c569
ManApart <akucera@pillartechnology.com>
2019-10-14 11:57:33 -0400

    update runtime config

--- rel/runtime.exs
+++ rel/runtime.exs
@@ -1,5 +1,14 @@
 use Mix.Config
 
+kafka_brokers = System.get_env("KAFKA_BROKERS")
+
+endpoint =
+  kafka_brokers
+  |> String.split(",")
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(fn entry -> String.split(entry, ":") end)
+  |> Enum.map(fn [host, port] -> {String.to_atom(host), String.to_integer(port)} end)
+
 config :discovery_api, DiscoveryApiWeb.Endpoint,
   url: [
     scheme: "https",
