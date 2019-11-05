28b2a6f131b7b8b5bc5e7bfb27f85213da856460
Jake Prem <jakeprem@gmail.com>
2019-11-05 16:10:00 -0500

    working-ish solution to roll back UUID PK for users table
    smartcitiesdata/discovery_ui#14
    
    co-authored-by: Nathaniel Stevens <nstevens@pillartechnology.com>

--- priv/repo/migrations/20191105155300_user_uuid_primary_key.exs
+++ priv/repo/migrations/20191105155300_user_uuid_primary_key.exs
@@ -7,17 +7,12 @@ defmodule DiscoveryApi.Repo.Migrations.UserUuidPrimaryKey do
   alias DiscoveryApi.Schemas.Visualizations.Visualization
 
   def up do
-    # create the new id column in users
     execute ~s|CREATE EXTENSION IF NOT EXISTS "uuid-ossp";|
 
     alter table(:users) do
       add(:new_id, :uuid, default: fragment("uuid_generate_v4()"))
     end
 
-    # create unique_index(:users, [:new_id])
-
-    # Add that column as new column to vizualization
-    # create a new foreign key constraint from visuzlizations
     alter table(:visualizations) do
       add :new_owner_id, :uuid
     end
@@ -27,27 +22,21 @@ defmodule DiscoveryApi.Repo.Migrations.UserUuidPrimaryKey do
     from(visualization in Visualization, update: [set: [new_owner_id: fragment("select users.new_id from users where users.id = ?", visualization.owner_id)]])
     |> Repo.update_all([])
 
-    # make new columns non-nullable
     alter table(:visualizations) do
       modify :new_owner_id, :uuid, null: false
     end
 
-    # drop the old foreign key constraint from visuzlizations
     alter table(:visualizations) do
       remove :owner_id
     end
 
-    # Delete old id column
-    # Change the primary key in users
     alter table(:users) do
       remove :id
       modify :new_id, :uuid, primary_key: true
     end
 
-    # rename the new id column
     rename(table(:users), :new_id, to: :id)
 
-    # rename new column in Vizualization
     rename(table(:visualizations), :new_owner_id, to: :owner_id)
 
     alter table(:visualizations) do
@@ -56,20 +45,38 @@ defmodule DiscoveryApi.Repo.Migrations.UserUuidPrimaryKey do
   end
 
   def down do
-    # alter table(:users) do
-    #   add :id, :integer
-    # end
+    alter table(:users) do
+      add(:new_id, :serial)
+    end
 
-    # alter table(:visualizations) do
-    #   add :owner_id, references(:users, type: :integer, column: :id)
-    # end
+    alter table(:visualizations) do
+      add :new_owner_id, :integer
+    end
 
-    # alter table(:visualizations) do
-    #   remove :new_owner_id
-    # end
+    flush()
 
-    # alter table(:users) do
-    #   remove :new_id
-    # end
+    from(visualization in Visualization, update: [set: [new_owner_id: fragment("select users.new_id from users where users.id = ?", visualization.owner_id)]])
+    |> Repo.update_all([])
+
+    alter table(:visualizations) do
+      modify :new_owner_id, :integer, null: false
+    end
+
+    alter table(:visualizations) do
+      remove :owner_id
+    end
+
+    alter table(:users) do
+      remove :id
+      modify :new_id, :integer, primary_key: true
+    end
+
+    rename(table(:users), :new_id, to: :id)
+
+    rename(table(:visualizations), :new_owner_id, to: :owner_id)
+
+    alter table(:visualizations) do
+      modify :owner_id, references(:users, type: :integer)
+    end
   end
 end
