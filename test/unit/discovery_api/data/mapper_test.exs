de8ea74d788a4838bb55bdec4f0fda7ff104345c
mcsearchin <abrock17@gmail.com>
2019-11-08 10:52:01 -0500

    Addressed failing test for mime-type override in SmartCity.Dataset; logging failures in event handler
    smartcitiesdata/discovery_ui#14
    
    co-authored-by: Nathaniel Stevens <nstevens@pillartechnology.com>

--- test/unit/discovery_api/data/mapper_test.exs
+++ test/unit/discovery_api/data/mapper_test.exs
@@ -7,7 +7,12 @@ defmodule DiscoveryApi.Data.MapperTest do
 
   describe "to_data_model/2 hard overrides" do
     data_test "with #{inspect(overrides)} should default #{inspect(field)} to #{inspect(value)}" do
-      dataset = TDG.create_dataset(overrides)
+      dataset =
+        overrides
+        |> TDG.create_dataset()
+        # NOTE: *for now* we need to re-apply the overrides to make sure that our provided source format does not get converted to a mime type
+        # that we're not using yet in Discovery API.  This will change once we start consuming dataset update events from the event stream.
+        |> SmartCity.Helpers.deep_merge(overrides)
       organization = TDG.create_schema_organization(%{})
 
       %Model{} = result = Mapper.to_data_model(dataset, organization)
@@ -24,7 +29,6 @@ defmodule DiscoveryApi.Data.MapperTest do
         [%{technical: %{private: true}}, [:accessLevel], "non-public"],
         [%{business: %{conformsToUri: nil}}, [:conformsToUri], "https://project-open-data.cio.gov/v1.1/schema/"],
         [%{business: %{conformsToUri: "overridden"}}, [:conformsToUri], "https://project-open-data.cio.gov/v1.1/schema/"],
-        #TODO: fix - appears that the SmartCity upgrade broke these tests. Need to verify how they should be fixed.
         [%{technical: %{sourceFormat: "json"}}, [:fileTypes], ["JSON"]],
         [%{technical: %{sourceFormat: "gtfs"}}, [:fileTypes], ["JSON"]],
         [%{technical: %{sourceFormat: "cSv"}}, [:fileTypes], ["CSV"]]
