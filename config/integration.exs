2aa25081e6116aa4e0d54124ed4104df423957a5
Christoph <ignatz@gmail.com>
2019-11-13 09:29:13 -0500

    Removed errant comma

--- config/integration.exs
+++ config/integration.exs
@@ -21,7 +21,7 @@ config :discovery_api,
 config :discovery_api, DiscoveryApi.Auth.Auth0.Guardian, issuer: "https://smartcolumbusos-demo.auth0.com/"
 
 config :redix,
-  args: [host: host],
+  args: [host: host]
 
 config :phoenix,
   serve_endpoints: true,
